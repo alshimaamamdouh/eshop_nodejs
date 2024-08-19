aa37e7909050a74cd441f92c6ad08e03c775273c
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-08-19 23:15:25 +0300

    19/8/2024

--- backend/models/cart.js
+++ backend/models/cart.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CartItem' }]
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'CartItem' }],
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  quantity: { type: Number, required: true, min: 1 }
 }, { timestamps: true });
 
 
